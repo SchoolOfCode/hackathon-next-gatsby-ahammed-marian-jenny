6ceb256e6035e0eb10255cf3f2ccd72bdb3a928f
Jenny Taylor <jennytaylor82@gmail.com>
2022-01-28 16:07:59 +0000

    amended css for search page results

--- my-app/pages/useEffect.js
+++ my-app/pages/useEffect.js
@@ -2,6 +2,8 @@
 import { useState, useEffect } from 'react';
 import Navbar from "../src/components/Navbar";
 import style from "../styles/useEffect.module.css"
+import styles from '../styles/about.module.css'
+
 export default function StarInput() {
     const [nameInput, setNameInput] = useState(false);
     const [post, setPost] = useState([]);
@@ -37,10 +39,10 @@ export default function StarInput() {
             <input placeholder="Author"/>
             <button>Search for Title</button>
         </form>
-        <ul>
+        <ul className={styles.listItem }>
         {post.map((e, index)=>{
                 if(index < 10){
-                    return <li key={index}> {e.title}</li> 
+                    return <li className={ styles.li} key={index}> {e.title}</li> 
                 }   
             }
             )}
