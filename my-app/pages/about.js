44287e295d01c5250ebafbc5452a2e8d5b8f8e0a
Jenny Taylor <jennytaylor82@gmail.com>
2022-01-28 16:03:01 +0000

    about page added

--- my-app/pages/about.js
+++ my-app/pages/about.js
@@ -1,25 +1,17 @@
-import React, { useState } from "react";
-import authors from "./data";
 import Navbar from "../src/components/Navbar";
-import style from "../styles/about.module.css";
+import style from '../styles/about.module.css'
 
 function about() {
-  const [author, setAuthor] = useState(authors);
-
   return (
     <div>
-      <div className={style.navbar}>
-        <Navbar />
-      </div>
-      <div>
-        <ul className={style.listItem}>
-          {author.map((e, index) => (
-            <li className={style.li} key={index}>{e.name}</li>
-          ))}
-        </ul>
-      </div>
+      <Navbar />
+          <h3 className={style.text}>
+              This is our fabulous poem finder.</h3>
+          <h3 className={style.text}>Click on the "List of Poets" to browse
+        all the poets in our database.</h3><h3 className={style.text}> Search by poet name in the "Search by
+        Poet" page to reveal a list of up to 10 poems by that poet.
+      </h3>
     </div>
   );
 }
-
 export default about;
